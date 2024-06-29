b40db0e500ea18449056f5e3793af82601fecbe2
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-06-29 15:52:35 +0530

    feat: Update navbar to be responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -68,7 +68,7 @@ export default function Navbar() {
         width={168}
         height={66}
       />
-      <ul className="hidden flex-row justify-center items-center gap-12 text-white">
+      <ul className="hidden md:flex flex-row justify-center items-center gap-12 text-white">
         {navItems.map((item) => (
           <li key={item.name}>
             <Link
@@ -82,102 +82,26 @@ export default function Navbar() {
         ))}
       </ul>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div className="p-2 border-[#484848] border rounded cursor-pointer">
-            <TiThMenu className="text-white " />
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 border-[#484848] border rounded bg-[#1E1E1E] text-white relative bottom-10 right-3">
-          <ul className="flex-col flex justify-center items-start">
-            {navItems.map((item) => (
-              <Link key={item.name} href={item.href} className="w-full">
-                <li className="transition-colors px-[1.5rem] py-[.625rem] duration-300">
-                  {item.name}
-                </li>
-              </Link>
-            ))}
-          </ul>
-          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <User className="mr-2 h-4 w-4" />
-              <span>Profile</span>
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <CreditCard className="mr-2 h-4 w-4" />
-              <span>Billing</span>
-              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Settings className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Keyboard className="mr-2 h-4 w-4" />
-              <span>Keyboard shortcuts</span>
-              <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <Users className="mr-2 h-4 w-4" />
-              <span>Team</span>
-            </DropdownMenuItem>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <UserPlus className="mr-2 h-4 w-4" />
-                <span>Invite users</span>
-              </DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                  <DropdownMenuItem>
-                    <Mail className="mr-2 h-4 w-4" />
-                    <span>Email</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <MessageSquare className="mr-2 h-4 w-4" />
-                    <span>Message</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>
-                    <PlusCircle className="mr-2 h-4 w-4" />
-                    <span>More...</span>
-                  </DropdownMenuItem>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuItem>
-              <Plus className="mr-2 h-4 w-4" />
-              <span>New Team</span>
-              <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Github className="mr-2 h-4 w-4" />
-            <span>GitHub</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <LifeBuoy className="mr-2 h-4 w-4" />
-            <span>Support</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem disabled>
-            <Cloud className="mr-2 h-4 w-4" />
-            <span>API</span>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-          </DropdownMenuItem> */}
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="p-2 border-[#484848] border rounded cursor-pointer">
+              <TiThMenu className="text-white " />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56 border-[#484848] border rounded bg-[#1E1E1E] text-white relative bottom-10 right-3">
+            <ul className="flex-col flex justify-center items-start">
+              {navItems.map((item) => (
+                <Link key={item.name} href={item.href} className="w-full">
+                  <li className="transition-colors px-[1.5rem] py-[.625rem] duration-300">
+                    {item.name}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </nav>
   );
 }
