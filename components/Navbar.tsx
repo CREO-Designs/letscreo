2f12c9eb5a839cd2d1317fbca2f1e5fa73354f16
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-07-03 20:58:19 +0530

    fix: Improve responsiveness of Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -60,46 +60,47 @@ export default function Navbar() {
     },
   ];
   return (
-    <nav className="fixed flex w-full items-center justify-between bg-[#171717] px-4 py-[.62rem] md:px-14">
-      <Image
-        className="h-[1.875rem] w-[4.9375rem] md:h-[3.125rem] md:w-[9.375rem] 2xl:h-[4.125rem] 2xl:w-[10.5rem]"
-        src={logo}
-        alt="CREO logo"
-        width={168}
-        height={66}
-      />
-      <ul className="hidden flex-row items-center justify-center gap-12 text-white md:flex">
-        {navItems.map((item) => (
-          <li key={item.name}>
-            <Link
-              href={item.href}
-              className="text-white transition-colors duration-300 hover:font-extrabold"
-            >
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      <div className="md:hidden">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div className="cursor-pointer rounded border border-[#484848] p-2">
-              <TiThMenu className="text-white" />
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="relative bottom-10 right-3 w-56 rounded border border-[#484848] bg-[#1E1E1E] text-white">
-            <ul className="flex flex-col items-start justify-center">
-              {navItems.map((item) => (
-                <Link key={item.name} href={item.href} className="w-full">
-                  <li className="px-[1.5rem] py-[.625rem] transition-colors duration-300">
-                    {item.name}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <nav className="fixed flex w-full items-center justify-center bg-[#171717]">
+      <div className="flex w-full max-w-[1780px] items-center justify-between px-4 py-[.62rem] md:px-14">
+        <Image
+          className="h-[1.875rem] w-[4.9375rem] md:h-[3.125rem] md:w-[9.375rem] 2xl:h-[4.125rem] 2xl:w-[10.5rem]"
+          src={logo}
+          alt="CREO logo"
+          width={168}
+          height={66}
+        />
+        <ul className="hidden flex-row items-center justify-center gap-12 text-white md:flex">
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <Link
+                href={item.href}
+                className="text-white transition-colors duration-300 hover:font-extrabold"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div className="cursor-pointer rounded border border-[#484848] p-2">
+                <TiThMenu className="text-white" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="relative bottom-10 right-3 w-56 rounded border border-[#484848] bg-[#1E1E1E] text-white">
+              <ul className="flex flex-col items-start justify-center">
+                {navItems.map((item) => (
+                  <Link key={item.name} href={item.href} className="w-full">
+                    <li className="px-[1.5rem] py-[.625rem] transition-colors duration-300">
+                      {item.name}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </nav>
   );
