75bf71282dc1333fd5aa5c6f4a53ea0190a9678d
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-07-03 20:58:43 +0530

    feat: Update landing page with HeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-import Image from "next/image";
-import banner from "@/public/banner.png";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="w-full font-gilroy max-w-[100rem] mx-auto">
-      {/* padding on top for navbar display */}
-      {/* <section className="pt-[95.65px] px-36 bg-[#1E1E1E] text-white h-dvh flex justify-between items-center">
-        <div>
-          <h1 className="text-white font-gilroy text-8xl font-extrabold leading-[100px]">
-            Where Creativity
-            <br />
-            Meets Growth
-          </h1>
-          <h2 className="w-[676px] text-white text-2xl leading-10">
-            We meet all your creative needs by providing you with your own
-            dedicated Design Team, delivering customized assets tailored just
-            for you.
-          </h2>
-          <button>Contact Us</button>
-        </div>
-        <Image
-          className="rounded-[10px] border-4 border-white"
-          src={banner}
-          alt="CREO banner"
-          width={409}
-          height={490}
-        />
-      </section> */}
+    <main className="mx-auto w-full max-w-[1780px] font-gilroy">
+      <HeroSection />
     </main>
   );
 }
---
+++ components/HeroSection.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import banner from "@/public/banner.png";
+
+export default function HeroSection() {
+  return (
+    <>
+      {/* 95.65px padding on top for navbar display */}
+      <section
+        id="home"
+        className="flex w-full flex-col justify-center bg-[#1E1E1E] px-6 pb-9 pt-[5.589375rem] text-white sm:pb-[4.5rem] md:flex-row md:items-center md:justify-between md:gap-12 md:px-[5.25rem] md:pt-[6.839375rem] 2xl:px-[8.75rem] 2xl:pt-[7.839375rem]"
+      >
+        <div className="mb-9 flex flex-col items-center md:mb-0 md:items-start">
+          <h1 className="mb-6 text-center text-[28px] font-extrabold leading-10 text-white sm:mb-8 sm:text-[70px] sm:leading-[90px] md:text-start 2xl:mb-[3.375rem] 2xl:text-8xl 2xl:leading-[100px]">
+            Where Creativity
+            <br />
+            Meets Growth
+          </h1>
+          <h2 className="text-md mb-6 text-center font-light leading-loose text-white sm:mb-8 sm:text-lg sm:leading-[30px] md:text-start 2xl:mb-[3.375rem] 2xl:text-2xl 2xl:leading-10">
+            We meet all your creative needs by providing you with your own
+            dedicated Design Team, delivering customized assets tailored just
+            for you.
+          </h2>
+          <button className="w-fit rounded-sm bg-rose-500 px-5 py-2 text-center font-extrabold leading-3 text-white sm:mb-14 sm:text-xl">
+            Contact Us
+          </button>
+        </div>
+        <Image
+          className="aspect-[261/142] h-fit w-full rounded-[10px] border-4 border-white bg-no-repeat object-cover object-center md:aspect-[409/490] md:w-[25.5625rem]"
+          src={banner}
+          alt="CREO banner"
+          width={409}
+          height={490}
+        />
+      </section>
+    </>
+  );
+}
