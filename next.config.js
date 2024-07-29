02b4f27d602c481ba748cf27f7b4d63c29bff7fd
Swapnil Raj <raj.swapnil1708@gmail.com>
2024-07-29 15:05:34 +0530

    config: Add exception for s3 bucket image urls in next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "letscreotest.s3.ap-south-1.amazonaws.com",
+        port: "",
+        //   pathname: '/account123/**',
+      },
+    ],
+  },
+};
